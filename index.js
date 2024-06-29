af0bd1ac06fae68a47f3606a7df6bd997196856e
kevin <kipronok234@gmail.com>
2024-06-29 22:54:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function(activity = 'go to the office'){
+   return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(wrapper = '*'){
+    const name = "You are"
+    return function(adjective = 'special'){
+     return `${name} ${wrapper}${adjective}${wrapper}!`;   
+    }
+    wrapAdjective(adjective)('hardworker');
+}
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+};
+
